8149886f760aa9d66c216f8381c2c89724aa15fd
Olexandr194 <olexandrmirochnik@gmail.com>
2022-07-04 12:12:37 +0300

    add seeds for products

---
+++ database/seeders/ProductsTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($i = 0; $i <10; $i++)
+        DB::table('products')->insert([
+            'title' => 'Product ' . $i,
+            'price' => rand(100, 2000),
+            'description' => 'Some seeding operations may cause you to alter or lose data. In order to protect you from running seeding commands against your production database, you will be prompted for confirmation before the seeders are executed in the production environment. To force the seeders to run without a prompt, use the --force flag',
+            'image' => 'image' . $i,
+            'quantity' => rand(10, 20),
+            'is_available' => 1,
+            'is_published' => 1,
+            'category_id' => rand(1, 3),
+        ]);
+    }
+}
