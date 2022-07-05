01ee78d0aa66d0a9797fb226d751f1e3bcd859d2
Olexandr194 <olexandrmirochnik@gmail.com>
2022-07-05 16:19:14 +0300

    add Admin LTE

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function __invoke(){
+
+        return 111111;
+    }
+}
